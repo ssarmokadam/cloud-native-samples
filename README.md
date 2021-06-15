dbc536af4aa063a41e7c38d7621790b87601ae43
Sarmokadam <sneha.a.sarmokadam@capgemini.com>
2021-06-15 12:59:19 +0530

    adding sample applications and documentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cloud-native-samples
 To try cloud native sample
+
+demo: It is plain vanilla SpringBoot Application. It is created from https://start.spring.io/
+
+devon4j-cloud-native: It is devon4j sample application where I have added experimental dependency of spring cloud native.
---
+++ devon4j-cloud-native/core/src/main/java/com/devonfw/devon4j_cloud_native/general/logic/base/AbstractComponentFacade.java
@@ -0,0 +1,16 @@
+package com.devonfw.devon4j_cloud_native.general.logic.base;
+
+/**
+ * Abstract base class for any component implementation class in this application.
+ */
+public abstract class AbstractComponentFacade extends AbstractLogic {
+
+  /**
+   * The constructor.
+   */
+  public AbstractComponentFacade() {
+
+    super();
+  }
+
+}
